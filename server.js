bebf665a6ebfc99bc818ae7faf0a2a67d69c7d37
saber13812002 <saber.tabatabaee@gmail.com>
2020-06-03 20:56:55 +0430

    this is the first nodejs project that rest client used in vscode put in response a json static data

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hi")
+// console.log("Hi")
+
+const express = require("express");
+const app = express();
+
+const posts = [{ name: 1 }, { name: 2 }];
+
+app.get("/", (req, res) => {
+  res.json(posts);
+});
+
+app.listen(3000);
