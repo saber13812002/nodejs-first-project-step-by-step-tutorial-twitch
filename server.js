b0adaca9ee71f13ec84f5a9e8ca5d83ef2d47fda
saber13812002 <saber.tabatabaee@gmail.com>
2020-06-05 17:38:31 +0430

    authentication with auth token with another function called authToken and add it to the get method in apis

--- server.js
+++ server.js
@@ -6,20 +6,43 @@ const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 
-const posts = [{ name: 1 }, { name: 2 }];
+const posts = [
+  { user_id: 1, title: "saber post title", username: "saber" },
+  { user_id: 2, title: "ali post title", username: "ali" },
+];
 
-app.get("/", (req, res) => {
-  res.json(posts);
+app.get("/posts", authToken, (req, res) => {
+  //res.json(posts);
+  res.json(
+    posts.filter((post) => 
+      post.user_id == req.user.id
+      //post.username === req.user.name
+    )
+  );
 });
 
 app.post("/login", (req, res) => {
   // user auth
   const username = req.body.username;
-  const user = { name: username };
+  const id = req.body.id;
+  const user = { name: username, id: id };
 
   const jwt_token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
 
   res.json({ jwt: jwt_token });
 });
+
+function authToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(401);
+    //
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
